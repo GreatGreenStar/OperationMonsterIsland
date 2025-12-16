e652a6a20cf4c7106d30ee7d7d3296596f071714
GreatGreenStar <77800979+GreatGreenStar@users.noreply.github.com>
2025-12-16 15:50:42 -0600

    Add files via upload
    Changes files for new godot version

--- operationmonsterisland/Main.gd
+++ operationmonsterisland/Main.gd
@@ -8,15 +8,12 @@ var place : int = 0
 
 func _unhandled_input(event: InputEvent) -> void:
 	var tween = create_tween()
-	
-	if Input.is_action_just_pressed("ui_left"):
-		tween.tween_property(hokuToken, "position", Vector2(hokuToken.position.x - 10, hokuToken.position.y), 0.1)
-		
-	if Input.is_action_just_pressed("ui_right"):
-		tween.tween_property(hokuToken, "position", Vector2(hokuToken.position.x + 10, hokuToken.position.y), 0.1)
-	
-	if Input.is_action_just_pressed("ui_up"):
-		tween.tween_property(hokuToken, "position", Vector2(hokuToken.position.x, hokuToken.position.y - 10), 0.1)
-	
-	if Input.is_action_just_pressed("ui_down"):
-		tween.tween_property(hokuToken, "position", Vector2(hokuToken.position.x, hokuToken.position.y + 10), 0.1)
+	match event.as_text():
+		"Left":
+			tween.tween_property(hokuToken, "position", Vector2(hokuToken.position.x - 10, hokuToken.position.y), 0.1)
+		"Right":
+			tween.tween_property(hokuToken, "position", Vector2(hokuToken.position.x + 10, hokuToken.position.y), 0.1)
+		"Up":
+			tween.tween_property(hokuToken, "position", Vector2(hokuToken.position.x, hokuToken.position.y - 10), 0.1)
+		"Down":
+			tween.tween_property(hokuToken, "position", Vector2(hokuToken.position.x, hokuToken.position.y + 10), 0.1)
